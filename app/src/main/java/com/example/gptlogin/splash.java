e8c4f7148b5a38943732776c78287803d7df012d
rathodria <riyakamalrathod25@gmail.com>
2024-09-12 23:23:03 +0530

    commit

--- app/src/main/java/com/example/gptlogin/splash.java
+++ app/src/main/java/com/example/gptlogin/splash.java
@@ -1,19 +1,28 @@
 package com.example.gptlogin;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
     private static final int SPLASH_SCREEN_DURATION = 2000;
+    ImageView img;
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+        img = (ImageView)findViewById(R.id.imageView);
+        Animation animZoomIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadin);
+        img.startAnimation(animZoomIn);
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(splash.this, MainActivity.class);
             startActivity(intent);
