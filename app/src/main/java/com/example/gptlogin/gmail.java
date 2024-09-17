d61a32c0f67266c0126e949022ed645a12b571b8
DELL <dhruviptl265@gmail.com>
2024-09-17 11:10:09 +0530

    commited

--- app/src/main/java/com/example/gptlogin/gmail.java
+++ app/src/main/java/com/example/gptlogin/gmail.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gmail extends AppCompatActivity {
