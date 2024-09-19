a73f816931557c9b6c83314e9d13ad890631aa4b
DELL <dhruviptl265@gmail.com>
2024-09-19 22:21:27 +0530

    commited

--- app/src/main/java/com/example/gptlogin/splash.java
+++ app/src/main/java/com/example/gptlogin/splash.java
@@ -2,19 +2,21 @@ package com.example.gptlogin;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
     private static final int SPLASH_SCREEN_DURATION = 2000;
-    ImageView img;
-
+    ImageView im;
     @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +25,17 @@ public class splash extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             EdgeToEdge.enable(this);
             setContentView(R.layout.activity_splash);
-            img = (ImageView) findViewById(R.id.imageView);
+//            VideoView videoView=(VideoView)findViewById(R.id.vv);
+//            MediaController mediaController=new MediaController(this);
+//            mediaController.setAnchorView(videoView);
+//            Uri uri=Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
+//            videoView.setMediaController(mediaController);
+//            videoView.setVideoURI(uri);
+//            videoView.requestFocus();
+//            videoView.start();
+            im=(ImageView)findViewById(R.id.img);
             Animation animZoomIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadin);
-            img.startAnimation(animZoomIn);
+            im.startAnimation(animZoomIn);
             new Handler().postDelayed(() -> {
                 Intent intent = new Intent(splash.this, Home.class);
                 startActivity(intent);
