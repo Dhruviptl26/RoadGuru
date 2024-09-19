d63321d1368191a4f1f2c61e1e5bd28a0dfb6019
rathodria <riyakamalrathod25@gmail.com>
2024-09-19 22:25:32 +0530

    commit'

--- app/src/main/java/com/example/gptlogin/Home.java
+++ app/src/main/java/com/example/gptlogin/Home.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,12 +17,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity
+{
+    private Button searchFineBtn;
+    private EditText referenceNumber;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        searchFineBtn = findViewById(R.id.searchFineButton);
+        referenceNumber = findViewById(R.id.referenceNumber);
 
         // Set up ActionBar with custom layout
         ActionBar actionBar = getSupportActionBar();
@@ -46,7 +57,8 @@ public class Home extends AppCompatActivity {
                         }
                     });
 
-                    searchIcon.setOnClickListener(v -> {
+                    searchIcon.setOnClickListener(v ->
+                    {
                         Toast.makeText(Home.this, "Search icon clicked", Toast.LENGTH_SHORT).show();
                     });
                 } else {
@@ -58,6 +70,17 @@ public class Home extends AppCompatActivity {
         } else {
             Toast.makeText(this, "ActionBar is null", Toast.LENGTH_SHORT).show();
         }
+
+        // Add click listener for the Search Fine button
+        searchFineBtn.setOnClickListener(v -> {
+            String refNum = referenceNumber.getText().toString();
+            if (refNum.isEmpty()) {
+                Toast.makeText(Home.this, "Please insert a reference number", Toast.LENGTH_SHORT).show();
+            } else {
+                // Logic for searching fines with the reference number
+                Toast.makeText(Home.this, "Searching fines for reference: " + refNum, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Set up BottomNavigationView
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -71,8 +94,7 @@ public class Home extends AppCompatActivity {
                     Toast.makeText(Home.this, "Services selected", Toast.LENGTH_SHORT).show();
                     return true;
                 } else if (item.getItemId() == R.id.nav_901) {
-
-                    Intent intent=new Intent(Home.this, call.class);
+                    Intent intent = new Intent(Home.this, call.class);
                     startActivity(intent);
                     return true;
                 } else if (item.getItemId() == R.id.nav_more) {
